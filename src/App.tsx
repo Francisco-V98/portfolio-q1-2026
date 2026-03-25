640e5bbfde749c80de9612a684d82c7a7d840d85
Francisco <frncsico@gmail.com>
2026-03-24 21:22:38 -0400

    feat: Configure GitHub Pages deployment by adding `gh-pages` scripts, setting a base path in Vite, and switching to `HashRouter`.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import BusbanzProject from './pages/BusbanzProject';
 import AsesoriaProject from './pages/AsesoriaProject';
